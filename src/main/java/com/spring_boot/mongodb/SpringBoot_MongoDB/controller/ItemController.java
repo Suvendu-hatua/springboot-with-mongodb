1638e00c9bf7169a6bec1a6ed5719c3030ece2d9
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-14 19:49:29 +0530

    Implemented all the remaining HTTP methods in controller.

--- src/main/java/com/spring_boot/mongodb/SpringBoot_MongoDB/controller/ItemController.java
+++ src/main/java/com/spring_boot/mongodb/SpringBoot_MongoDB/controller/ItemController.java
@@ -3,10 +3,9 @@ package com.spring_boot.mongodb.SpringBoot_MongoDB.controller;
 import com.spring_boot.mongodb.SpringBoot_MongoDB.Pojo.Item;
 import com.spring_boot.mongodb.SpringBoot_MongoDB.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +19,43 @@ public class ItemController {
     }
 
     @GetMapping("/items")
-    public List<Item> getAllItems(){
-        return itemService.getAllItems();
+    public ResponseEntity<List<Item>> getAllItems(){
+        return ResponseEntity.ok(itemService.getAllItems());
     }
     @PostMapping("/items")
-    public Item addItems(@RequestBody Item item){
-        return itemService.addItem(item);
+    public ResponseEntity<Item> addItems(@RequestBody Item item){
+            Item result=itemService.addItem(item);
+            return ResponseEntity.ok(result);
+    }
+    @GetMapping("/items/{id}")
+    public ResponseEntity<?> getItemByID(@PathVariable String id){
+        try{
+            Item re=itemService.getItemById(id);
+            return ResponseEntity.ok(re);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("/items/{id}")
+    public ResponseEntity<Item> updateItem(@RequestBody Item item,@PathVariable String id){
+       try{
+           Item re=itemService.updateItem(id,item);
+           return ResponseEntity.ok(re);
+       }catch (Exception e){
+           return ResponseEntity.notFound().build();
+       }
+
+    }
+
+    @DeleteMapping("/items/{id}")
+    public ResponseEntity<?> deleteItemById(@PathVariable String id){
+        try{
+            itemService.deleteItemById(id);
+            return ResponseEntity.ok("successfully deleted.");
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
     }
 }
