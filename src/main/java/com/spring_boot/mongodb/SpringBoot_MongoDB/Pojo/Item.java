0348bc88e96d7c856bceb191b21db0940d593ea0
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-14 11:40:55 +0530

    Adding item Pojo and it's implementation.

---
+++ src/main/java/com/spring_boot/mongodb/SpringBoot_MongoDB/Pojo/Item.java
@@ -0,0 +1,55 @@
+package com.spring_boot.mongodb.SpringBoot_MongoDB.Pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "item")
+public class Item {
+    @Id
+    private int id;
+    private String title;
+    private String desc;
+    private float price;
+
+    //constructor
+
+    public Item() {
+    }
+
+    //Setter and getter
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    //toString()
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", desc='" + desc + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
